15020cc9a5459f61032fa6c95b9f16e8bdbfaa56
safarboyalkhwarizmi <safarboyalkhwarizmi@gmail.com>
2023-09-11 21:07:24 +0500

    TextInput values stored to variable

--- screens/Basket/ProductAddScreen.js
+++ screens/Basket/ProductAddScreen.js
@@ -1,186 +1,237 @@
 import React, { Component } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity, TextInput } from 'react-native';
-import { CheckBox } from 'react-native-elements'; // Import CheckBox from react-native-elements
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import { CheckBox } from 'react-native-elements';
 
 const screenWidth = Dimensions.get('window').width;
 
-let nds = false;
-
 class ProductAdd extends Component {
-    render() {
-        const { navigation } = this.props;
+  constructor(props) {
+    super(props);
+    this.state = {
+      seriyaInputValue: '',
+      brandInputValue: '',
+      productInputValue: '',
+      amountInputValue: '',
+      priceInputValue: '',
+      sellingPriceInputValue: '',
+      percentageInputValue: '',
+    };
+  }
 
-        return (
-            <>
-                <View style={styles.container}>
-                    <Text style={{ fontSize: 28, fontFamily: "Roboto-Black", marginBottom: 21 }}>Maxsulot qo’shish</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Brand nomi: "
-                        placeholderTextColor="black"
-                    />
+  handleButtonClick = () => {
+    const {
+      seriyaInputValue,
+      brandInputValue,
+      productInputValue,
+      amountInputValue,
+      priceInputValue,
+      sellingPriceInputValue,
+      percentageInputValue,
+    } = this.state;
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Maxsulot nomi: "
-                        placeholderTextColor="black"
-                    />
+    console.log('Seriya Value:', seriyaInputValue);
+    console.log('Brand Value:', brandInputValue);
+    console.log('Product Value:', productInputValue);
+    console.log('Amount Value:', amountInputValue);
+    console.log('Price Value:', priceInputValue);
+    console.log('Selling Price Value:', sellingPriceInputValue);
+    console.log('Percentage Value:', percentageInputValue);
 
-                    <View style={styles.amountGroup}>
-                        <TextInput
-                            style={styles.amountInput}
-                            placeholder="Maxsulot miqdori: "
-                            placeholderTextColor="black"
-                        />
-                        <View style={styles.amountType}>
-                            <Text style={{ color: "white", fontSize: 18, fontFamily: "Roboto-Regular" }}>KG</Text>
-                        </View>
-                    </View>
+    // You can do further processing with the values here
+  };
 
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Tan narxi: "
-                        placeholderTextColor="black"
-                    />
+  render() {
+    const { navigation } = this.props;
 
-                    <View style={styles.inputGroup}>
-                        <TextInput
-                            style={styles.priceInput}
-                            placeholder="Sotilish narxi: "
-                            placeholderTextColor="black"
-                        />
+    return (
+      <>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text style={{ fontSize: 28, fontFamily: "Roboto-Black", marginBottom: 21 }}>Maxsulot qo’shish</Text>
+          <TextInput
+              style={styles.input}
+              placeholder="Seriya"
+              placeholderTextColor="black"
+              onChangeText={(text) => this.setState({ seriyaInputValue: text })}
+          />
 
-                        <Text style={{ fontSize: 18, fontFamily: 'Roboto-Regular' }}>yoki</Text>
+          <TextInput
+              style={styles.input}
+              placeholder="Brand nomi: "
+              placeholderTextColor="black"
+              onChangeText={(text) => this.setState({ brandInputValue: text })}
+          />
 
-                        <TextInput
-                            style={styles.percentageInput}
-                            placeholder="%"
-                            placeholderTextColor="black"
-                        />
-                    </View>
+          <TextInput
+              style={styles.input}
+              placeholder="Maxsulot nomi: "
+              placeholderTextColor="black"
+              onChangeText={(text) => this.setState({ productInputValue: text })}
+          />
 
-                    <View style={{ display: "flex", flexDirection: "row", alignItems: "center", width: screenWidth - (17 + 17) }}>
-                        <CheckBox
-                            checked={nds}
-                            onPress={() => {nds = true}}
-                            containerStyle={{ margin: 0, padding: 0, borderWidth: 0, backgroundColor: 'transparent' }}
-                        />
-                        <Text style={{ fontSize: 18, fontFamily: 'Roboto-Regular' }}>NDS soliq</Text>
-                    </View>
+          <View style={styles.amountGroup}>
+            <TextInput
+                style={styles.amountInput}
+                placeholder="Maxsulot miqdori: "
+                placeholderTextColor="black"
+                onChangeText={(text) => this.setState({ amountInputValue: text })}
+            />
+            <View style={styles.amountType}>
+              <Text style={{ color: "white", fontSize: 18, fontFamily: "Roboto-Regular" }}>KG</Text>
+            </View>
+          </View>
 
-                    <View style={styles.buttons}>
-                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Basket')}>
-                            <Text style={styles.buttonText}>Bekor qilish</Text>
-                        </TouchableOpacity>
+          <TextInput
+              style={styles.input}
+              placeholder="Tan narxi: "
+              placeholderTextColor="black"
+              onChangeText={(text) => this.setState({ priceInputValue: text })}
+          />
 
-                        <TouchableOpacity style={styles.button}>
-                            <Text style={styles.buttonText}>Saqlash</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </>
-        );
-    }
+          <View style={styles.inputGroup}>
+            <TextInput
+                style={styles.priceInput}
+                placeholder="Sotilish narxi: "
+                placeholderTextColor="black"
+                onChangeText={(text) => this.setState({ sellingPriceInputValue: text })}
+            />
+
+            <Text style={{ fontSize: 18, fontFamily: 'Roboto-Regular' }}>yoki</Text>
+
+            <TextInput
+                style={styles.percentageInput}
+                placeholder="%"
+                placeholderTextColor="black"
+                onChangeText={(text) => this.setState({ percentageInputValue: text })}
+            />
+          </View>
+
+          <View style={{ display: "flex", flexDirection: "row", alignItems: "center", width: screenWidth - (17 + 17) }}>
+            <CheckBox
+                checked={true}
+                // onPress={() => {nds = true}}
+                containerStyle={{ margin: 0, padding: 0, borderWidth: 0, backgroundColor: 'transparent' }}
+            />
+            <Text style={{ fontSize: 18, fontFamily: 'Roboto-Regular' }}>NDS soliq</Text>
+          </View>
+
+          <View style={styles.buttons}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Basket')}>
+              <Text style={styles.buttonText}>Bekor qilish</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={this.handleButtonClick}>
+              <Text style={styles.buttonText}>Saqlash</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </>
+    );
+  }
 }
 
-const styles= StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: "center",
-        paddingTop: 70,
-        position: "relative",
-        gap: 10
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    paddingTop: 70,
+    position: "relative",
+    gap: 10
+  },
 
-    input: {
-        height: 64,
-        width: screenWidth - (17 + 17),
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingVertical: 23,
-        paddingHorizontal: 20,
-        fontSize: 18,
-        fontFamily: 'Roboto-Regular',
-    },
+  input: {
+    height: 64,
+    width: screenWidth - (17 + 17),
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingVertical: 23,
+    paddingHorizontal: 20,
+    fontSize: 18,
+    fontFamily: 'Roboto-Regular'
+  },
 
-    inputGroup: {
-        width: screenWidth - (17 + 17),
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between"
-    },
+  inputGroup: {
+    width: screenWidth - (17 + 17),
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
 
-    priceInput: {
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingVertical: 23,
-        paddingHorizontal: 20,
-        width: "50%",
-        fontSize: 18,
-        fontFamily: 'Roboto-Regular',
-    },
+  priceInput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingVertical: 23,
+    paddingHorizontal: 20,
+    width: "50%",
+    fontSize: 18,
+    fontFamily: 'Roboto-Regular',
+  },
 
-    percentageInput: {
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingVertical: 23,
-        paddingHorizontal: 20,
-        width: "30%",
-        fontSize: 18,
-        fontFamily: 'Roboto-Regular',
+  percentageInput: {
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingVertical: 23,
+    paddingHorizontal: 20,
+    width: "30%",
+    fontSize: 18,
+    fontFamily: 'Roboto-Regular',
+  },
 
+  amountGroup: {
+    display: "flex",
+    flexDirection: "row"
+  },
 
-    },
+  amountInput: {
+    borderWidth: 1,
+    paddingVertical: 23,
+    paddingHorizontal: 20,
+    width: screenWidth - (17 + 17 + 90),
+    fontSize: 18,
+    fontFamily: 'Roboto-Regular',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10
+  },
 
-    amountGroup: {
-        display: "flex",
-        flexDirection: "row"
-    },
+  amountType: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "black",
+    justifyContent: "center",
+    width: 90,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10
+  },
 
-    amountInput: {
-        borderWidth: 1,
-        paddingVertical: 23,
-        paddingHorizontal: 20,
-        width: screenWidth - (17 + 17 + 90),
-        fontSize: 18,
-        fontFamily: 'Roboto-Regular',
-        borderTopLeftRadius: 10,
-        borderBottomLeftRadius: 10
-    },
+  buttons: {
+    width: screenWidth - (17 + 17),
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 
-    amountType: {
-        display: "flex",
-        alignItems: "center",
-        backgroundColor: "black",
-        justifyContent: "center",
-        width: 90,
-        borderTopRightRadius: 10,
-        borderBottomRightRadius: 10
-    },
+  button: {
+    backgroundColor: "black",
+    paddingVertical: 16,
+    paddingHorizontal: 47,
+    borderRadius: 10
+  },
 
-    buttons: {
-        width: screenWidth - (17 + 17),
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-
-    button: {
-        backgroundColor: "black",
-        paddingVertical: 16,
-        paddingHorizontal: 47,
-        borderRadius: 10
-    },
-
-    buttonText: {
-        color: "white",
-        fontFamily: "Roboto-Bold",
-        fontSize: 16
-    }
+  buttonText: {
+    color: "white",
+    fontFamily: "Roboto-Bold",
+    fontSize: 16
+  }
 });
 
 export default ProductAdd;
