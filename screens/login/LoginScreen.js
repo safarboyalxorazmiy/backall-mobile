98a949cb2b4a9a608d1a6580a45f11e58da057a4
Safarboy <safarboyalxorazmiy@gmail.com>
2024-02-22 00:08:45 +0500

    Logging in section code redesigned and some update in api section

--- screens/login/LoginScreen.js
+++ screens/login/LoginScreen.js
@@ -1,16 +1,5 @@
-import React, {useState} from 'react';
-import {StatusBar} from 'expo-status-bar';
-import {
-	StyleSheet,
-	Text,
-	View,
-	TextInput,
-	Dimensions,
-	TouchableOpacity,
-	ScrollView,
-	Platform,
-} from 'react-native';
-
+import React, { Component } from 'react';
+import { StatusBar, StyleSheet, Text, View, TextInput, Dimensions, TouchableOpacity, ScrollView, Platform } from 'react-native';
 import Logo from '../../assets/logo.svg';
 import TokenService from '../../service/TokenService';
 import DatabaseService from '../../service/DatabaseService';
@@ -23,175 +12,158 @@ const databaseService = new DatabaseService();
 const tokenService = new TokenService();
 const apiService = new ApiService();
 
-const LoginForm = ({onSubmit}) => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	
-	return (
-		<View style={styles.form}>
-			<Text style={styles.label}>Loginni kiriting</Text>
-			<TextInput
-				style={styles.input}
-				placeholder="admin"
-				placeholderTextColor="black"
-				value={email}
-				onChangeText={(text) => setEmail(text)}
-			/>
-			
-			<Text style={styles.label}>Parolni kiriting</Text>
-			<TextInput
-				style={styles.input}
-				placeholder="********"
-				placeholderTextColor="black"
-				secureTextEntry
-				value={password}
-				onChangeText={(text) => setPassword(text)}
-			/>
-			
-			<TouchableOpacity onPress={() => onSubmit(email, password)}>
-				<View style={styles.button}>
-					<Text style={styles.buttonText}>Kirish</Text>
-				</View>
-			</TouchableOpacity>
-		</View>
-	);
-};
+class Login extends Component {
+    constructor(props) {
+			super(props);
+			this.state = {
+					email: '',
+					password: '',
+					loading: false
+			};
+    }
 
-const ForgotPasswordLink = ({onPress}) => (
-	<TouchableOpacity onPress={onPress} style={{marginTop: 168}}>
-		<View style={styles.forgotPasswordLink}>
-			<Text style={styles.forgotPasswordText}>Parolni unutdingizmi?</Text>
-		</View>
-	</TouchableOpacity>
-);
+    login = async () => {
+			const { email, password } = this.state;
+			try {
+				this.setState({ loading: true });
+				const result = await apiService.login(email + "@backall.uz", password);
+				console.log(result);
 
-const Login = ({navigation}) => {
-	let accessToken = "";
-	const [loading, setLoading] = useState(false);
-	
-	const login = async (email, password) => {
-		navigation.navigate("Verification");
+				if (result.access_token && result.refresh_token) {
+						await tokenService.storeAccessToken(result.access_token);
+						await tokenService.storeRefreshToken(result.refresh_token);
+				}
 
-		/*try {
-			setLoading(true);
-			
-			let result;
-			result.access_token = null;
-			result.refresh_token = null;
-			
-			result = await apiService.login(email, password);
-			
-			navigation.navigate("Home");
-			
-			if (
-				result.access_token != null &&
-				result.refresh_token != null
-			) {
-				await tokenService.storeAccessToken(result.access_token);
-				await tokenService.storeRefreshToken(result.refresh_token);
+				const accessToken = await tokenService.retrieveAccessToken();
+				console.log(accessToken);
+				console.log(await tokenService.retrieveRefreshToken());
+
+				await this.getProducts();
+				this.props.navigation.navigate("Verification");
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			} finally {
+				this.setState({ loading: false });
 			}
-			
-			accessToken = await tokenService.retrieveAccessToken();
-			console.log(accessToken);
-			console.log(await tokenService.retrieveRefreshToken());
-			
-			await getProducts();
-		} catch (error) {
-			console.error('Error fetching data:', error);
-		} finally {
-			setLoading(false);
-		}*/
-	};
-	
-	const getProducts = async () => {
-		fetch("http://backall.uz/api/v1/product/get/all", {
-			method: "GET",
-			headers: {
-				"Authorization": "Bearer " + accessToken
+    };
+
+    getProducts = async () => {
+			const accessToken = await tokenService.retrieveAccessToken();
+			try {
+				let result = await apiService.getProducts();
+				if (result) {
+					databaseService.createProducts(result);
+				}
+			} catch (error) {
+				console.log('error', error);
 			}
-		})
-			.then(response => response.json())
-			.then(result => {
-				databaseService.createProducts(result);
-			})
-			.catch(error => console.log('error', error));
-	};
-	
-	return (
-		<ScrollView contentContainerStyle={styles.container}>
-			{Platform.OS === 'android' || Platform.OS === 'ios' ? (
-				<Logo 
-					style={styles.logo} 
-					resizeMode="cover"/>
-			) : (
-				<Logo 
-					style={styles.logo}/>
-			)}
-			
-			<LoginForm 
-				onSubmit={login}/>
-			
-			<ForgotPasswordLink 
-				onPress={() => {
-					navigation.navigate('Home')
-				}}/>
-			
-			<StatusBar style="auto"/>
-		</ScrollView>
-	);
-};
+    };
+
+    forgotPasswordLink = () => {
+        this.props.navigation.navigate('Home');
+    };
+
+    render() {
+        const { email, password, loading } = this.state;
+        return (
+            <ScrollView contentContainerStyle={styles.container}>
+                {(Platform.OS === 'android' || Platform.OS === 'ios') ?
+                    <Logo style={styles.logo} resizeMode="cover" /> :
+                    <Logo style={styles.logo} />}
+                <View style={styles.form}>
+                    <Text style={styles.label}>Loginni kiriting</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="admin"
+                        placeholderTextColor="black"
+                        value={email}
+                        onChangeText={(text) => this.setState({ email: text })}
+                    />
+                    <Text style={styles.label}>Parolni kiriting</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="********"
+                        placeholderTextColor="black"
+                        secureTextEntry
+                        value={password}
+                        onChangeText={(text) => this.setState({ password: text })}
+                    />
+                    <TouchableOpacity onPress={async () => {
+											await this.login()
+										}}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Kirish</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity onPress={this.forgotPasswordLink} style={{ marginTop: 168 }}>
+                    <View style={styles.forgotPasswordLink}>
+                        <Text style={styles.forgotPasswordText}>Parolni unutdingizmi?</Text>
+                    </View>
+                </TouchableOpacity>
+                <StatusBar style="auto" />
+            </ScrollView>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		paddingTop: 104,
-		height: screenHeight
-	},
-	
-	logo: {
-		display: "block",
-		width: 220.313,
-		height: 96.563,
-		marginBottom: 83
-	},
-	
-	label: {
-		fontSize: 16,
-		fontWeight: '500',
-		fontFamily: 'Gilroy-Medium',
-		marginBottom: 4
-	},
-	
-	input: {
-		height: 64,
-		width: screenWidth - (24 + 24),
-		borderWidth: 1,
-		borderRadius: 10,
-		paddingVertical: 23,
-		paddingHorizontal: 20,
-		fontSize: 18,
-		marginBottom: 16,
-		fontFamily: 'Gilroy-Regular',
-	},
-	
-	button: {
-		width: screenWidth - (24 + 24),
-		backgroundColor: '#222',
-		color: 'white',
-		paddingVertical: 14,
-		borderRadius: 10,
-	},
-	
-	buttonText: {
-		color: 'white',
-		textAlign: 'center',
-		fontSize: 20,
-		textTransform: 'capitalize',
-		fontWeight: '500',
-		fontFamily: 'Gilroy-Medium'
-	},
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        paddingTop: 104,
+        height: screenHeight
+    },
+    logo: {
+        display: "block",
+        width: 220.313,
+        height: 96.563,
+        marginBottom: 83
+    },
+    form: {
+        alignItems: 'center'
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: '500',
+        fontFamily: 'Gilroy-Medium',
+        marginBottom: 4
+    },
+    input: {
+        height: 64,
+        width: screenWidth - (24 + 24),
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingVertical: 23,
+        paddingHorizontal: 20,
+        fontSize: 18,
+        marginBottom: 16,
+        fontFamily: 'Gilroy-Regular',
+    },
+    button: {
+        width: screenWidth - (24 + 24),
+        backgroundColor: '#222',
+        color: 'white',
+        paddingVertical: 14,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 20,
+        textTransform: 'capitalize',
+        fontWeight: '500',
+        fontFamily: 'Gilroy-Medium'
+    },
+    forgotPasswordLink: {
+        alignItems: 'center'
+    },
+    forgotPasswordText: {
+        fontSize: 16,
+        fontWeight: '500',
+        fontFamily: 'Gilroy-Medium'
+    }
 });
 
 export default Login;
--- service/ApiService.js
+++ service/ApiService.js
@@ -1,20 +1,37 @@
+const serverUrl = "http://192.168.0.100:8080";
+
 class ApiService {
 	constructor() {}
 	
 	async login(email, password) {
-		const response = await fetch('http://backall.uz/api/v1/auth/authenticate', {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		});
+		const response = 
+			await fetch(serverUrl + "/api/v1/auth/authenticate", {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: email,
+					password: password,
+				}),
+			});
 
 		return await response.json();
+	}
+
+	async getProducts(accessToken) {
+		try {
+			const response = await fetch(serverUrl + "/api/v1/product/get/all", {
+				method: "GET",
+				headers: {
+					"Authorization": "Bearer " + accessToken
+				}
+			});
+			return await response.json();
+		} catch (error) {
+			console.log('error', error);
+		}
 	}
 }
 
